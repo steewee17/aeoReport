76ba16fb4d04b08aff482b6cbe5d3e73349937f6
stefanwurzer <stefanwurzer.innos@gmail.com>
2026-04-24 20:02:56 +0200

    Delete dolomit/widget.v5.js

--- dolomit/widget.v5.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  var f = document.createElement('iframe');
-  f.src = 'https://aeo-report.com/dolomit/widget.v5.html';
-  f.style.cssText = 'position:fixed;bottom:0;right:0;width:460px;height:720px;border:none;z-index:2147483647;background:transparent;';
-  f.setAttribute('frameborder', '0');
-  f.setAttribute('scrolling', 'no');
-  f.setAttribute('allowtransparency', 'true');
-  document.body.appendChild(f);
-})();
