921b52724faa6e9e1501c22b04de6ff21c24fc9b
stefanwurzer <stefanwurzer.innos@gmail.com>
2026-04-24 20:24:36 +0200

    Add files via upload

---
+++ dolomit/widget.v8.js
@@ -0,0 +1,9 @@
+(function () {
+  var f = document.createElement('iframe');
+  f.src = 'https://aeo-report.com/dolomit/widget.v8.html';
+  f.style.cssText = 'position:fixed;bottom:0;right:0;width:420px;height:700px;border:none;z-index:2147483647;background:transparent;';
+  f.setAttribute('frameborder', '0');
+  f.setAttribute('scrolling', 'no');
+  f.setAttribute('allowtransparency', 'true');
+  document.body.appendChild(f);
+})();
