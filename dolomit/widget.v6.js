b9d9f8e154676290ff9bce2899762c0b4bebde4e
stefanwurzer <stefanwurzer.innos@gmail.com>
2026-04-24 20:08:47 +0200

    Update iframe source to widget.v6.html

--- dolomit/widget.v6.js
+++ dolomit/widget.v6.js
@@ -1,6 +1,6 @@
 (function () {
   var f = document.createElement('iframe');
-  f.src = 'https://aeo-report.com/dolomit/widget.v5.html';
+  f.src = 'https://aeo-report.com/dolomit/widget.v6.html';
   f.style.cssText = 'position:fixed;bottom:0;right:0;width:460px;height:720px;border:none;z-index:2147483647;background:transparent;';
   f.setAttribute('frameborder', '0');
   f.setAttribute('scrolling', 'no');
